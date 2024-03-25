7c51b7a0fefeb24c53da74c875951a5e10917dd2
rotsyrotsy <80813050+rotsyrotsy@users.noreply.github.com>
2024-03-25 10:08:28 +0300

    Ajout d'une page JSF pour afficher les détails d'un client

---
+++ src/main/java/com/mbds/tpcustomerapplication/jsf/CustomerDetailsBean.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.mbds.tpcustomerapplication.jsf;
+
+import com.mbds.tpcustomerapplication.entity.Customer;
+import com.mbds.tpcustomerapplication.service.CustomerManager;
+import jakarta.inject.Named;
+import java.io.Serializable;
+import jakarta.inject.Inject;
+import jakarta.faces.view.ViewScoped;
+
+/**
+ *  backing bean pour la page des détails
+ * @author Rotsy
+ */
+@Named(value = "customerDetailsBean")
+@ViewScoped
+public class CustomerDetailsBean implements Serializable {
+  private int idCustomer;
+  private Customer customer;
+
+  @Inject
+  private CustomerManager customerManager;
+
+  public int getIdCustomer() {
+    return idCustomer;
+  }
+
+  public void setIdCustomer(int idCustomer) {
+    this.idCustomer = idCustomer;
+  }
+
+  /**
+   * Retourne les détails du client courant (contenu dans l'attribut customer de
+   * cette classe).
+   */
+    public Customer getCustomer() {
+      return customer;
+    }
+
+  /**
+   * Action handler - met à jour dans la base de données les données du client
+   * contenu dans la variable d'instance customer.
+   * @return la prochaine page à afficher, celle qui affiche la liste des clients.
+   */
+  public String update() {
+    // Modifie la base de données.
+    // Il faut affecter à customer (sera expliqué dans le cours).
+    customer = customerManager.update(customer);
+    return "customerList";
+  }
+
+  public void loadCustomer() {
+    this.customer = customerManager.findById(idCustomer);
+  }
+}
--- src/main/java/com/mbds/tpcustomerapplication/service/CustomerManager.java
+++ src/main/java/com/mbds/tpcustomerapplication/service/CustomerManager.java
@@ -37,4 +37,8 @@ public class CustomerManager {
     public Customer update(Customer customer) {
         return em.merge(customer);
     }
+
+    public Customer findById(int idCustomer) {
+        return em.find(Customer.class, idCustomer);
+    }
 }
