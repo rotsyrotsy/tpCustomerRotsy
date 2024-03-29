b96186953833a904311936fa92d8f4772212aa1c
rotsyrotsy <80813050+rotsyrotsy@users.noreply.github.com>
2024-03-29 19:51:24 +0300

    Affichage des Discount dans la page des détails d'un client

--- src/main/java/com/mbds/tpcustomerapplication/jsf/CustomerDetailsBean.java
+++ src/main/java/com/mbds/tpcustomerapplication/jsf/CustomerDetailsBean.java
@@ -5,54 +5,71 @@
 package com.mbds.tpcustomerapplication.jsf;
 
 import com.mbds.tpcustomerapplication.entity.Customer;
+import com.mbds.tpcustomerapplication.entity.Discount;
 import com.mbds.tpcustomerapplication.service.CustomerManager;
+import com.mbds.tpcustomerapplication.service.DiscountManager;
 import jakarta.inject.Named;
 import java.io.Serializable;
 import jakarta.inject.Inject;
 import jakarta.faces.view.ViewScoped;
+import java.util.List;
 
 /**
- *  backing bean pour la page des détails
+ * backing bean pour la page des détails
+ *
  * @author Rotsy
  */
 @Named(value = "customerDetailsBean")
 @ViewScoped
 public class CustomerDetailsBean implements Serializable {
-  private int idCustomer;
-  private Customer customer;
 
-  @Inject
-  private CustomerManager customerManager;
+    private int idCustomer;
+    private Customer customer;
 
-  public int getIdCustomer() {
-    return idCustomer;
-  }
+    @Inject
+    private CustomerManager customerManager;
+    @Inject
+    private DiscountManager discountManager;
 
-  public void setIdCustomer(int idCustomer) {
-    this.idCustomer = idCustomer;
-  }
-
-  /**
-   * Retourne les détails du client courant (contenu dans l'attribut customer de
-   * cette classe).
-   */
-    public Customer getCustomer() {
-      return customer;
+    public int getIdCustomer() {
+        return idCustomer;
     }
 
-  /**
-   * Action handler - met à jour dans la base de données les données du client
-   * contenu dans la variable d'instance customer.
-   * @return la prochaine page à afficher, celle qui affiche la liste des clients.
-   */
-  public String update() {
-    // Modifie la base de données.
-    // Il faut affecter à customer (sera expliqué dans le cours).
-    customer = customerManager.update(customer);
-    return "customerList";
-  }
+    public void setIdCustomer(int idCustomer) {
+        this.idCustomer = idCustomer;
+    }
 
-  public void loadCustomer() {
-    this.customer = customerManager.findById(idCustomer);
-  }
+    /**
+     * Retourne les détails du client courant (contenu dans l'attribut customer
+     * de cette classe).
+     */
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    /**
+     * Action handler - met à jour dans la base de données les données du client
+     * contenu dans la variable d'instance customer.
+     *
+     * @return la prochaine page à afficher, celle qui affiche la liste des
+     * clients.
+     */
+    public String update() {
+        // Modifie la base de données.
+        // Il faut affecter à customer (sera expliqué dans le cours).
+        customer = customerManager.update(customer);
+        return "customerList";
+    }
+
+    public void loadCustomer() {
+        this.customer = customerManager.findById(idCustomer);
+    }
+
+    /**
+     * Retourne la liste de tous les Discount.
+     * 
+     */
+    public List<Discount> getDiscounts() {
+        return discountManager.getAllDiscounts();
+    }
 }
---
+++ src/main/java/com/mbds/tpcustomerapplication/service/DiscountManager.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mbds.tpcustomerapplication.service;
+
+import com.mbds.tpcustomerapplication.entity.Discount;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import java.util.List;
+
+/**
+ * Façade pour gérer les discount
+ * @author Rotsy
+ */
+@RequestScoped
+public class DiscountManager {
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+
+    public List<Discount> getAllDiscounts() {
+        Query query = em.createNamedQuery("Discount.findAll");
+        return query.getResultList();
+    }
+
+    public Discount findById(String code) {
+        return em.find(Discount.class, code);
+    }
+}
